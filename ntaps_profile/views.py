a35ecf23ecfb14ab7937c146e0404f69af8bf672
Mochaul <praditomo@gmail.com>
2017-10-09 11:02:32 +0700

    finish backend profile

--- ntaps_profile/views.py
+++ ntaps_profile/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 # Create your views here.
-response = {'author': "Hash Slinging Slasher"}
-about_me = ["Rusty spatula", "Red eyes", "A cook", "Male", "Ghost", "Clumsy"]
+response = {'name': "Hash Slinging Slasher", 'birthday': "29 Feb", 'gender': "Unknown", 'description': "A dead Krusty Krab's cook", 'email': "hashslinging@krustykrab.bb"}
+expertise = ["Rusty spatula", "Red eyes", "A cook", "Male", "Ghost", "Clumsy"]
 def index(request):
     html = 'profile/ntaps_profile.html'
-    response['about_me'] = about_me
+    response['expertise'] = expertise
     return render(request, html, response)

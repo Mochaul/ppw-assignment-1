c5e5b2931eb1c6ca370272384f10a98e75d3bf0b
Mochaul <praditomo@gmail.com>
2017-10-11 00:32:26 +0700

    fix models

--- ntaps_profile/models.py
+++ ntaps_profile/models.py
@@ -1,5 +1,11 @@
-from django.db import models
-
-# Create your models here.
 class Profile(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=64)
+    email = models.EmailField()
+    birth_date = models.DateField()
+    expertise =  models.TextField() # comma-separated
+    gender = models.CharField(max_length=8)
+    description = models.CharField(max_length=128)
+    picture_url = models.URLField()
+
+    def __str__(self):
+        return self.name + '(' + self.email + ')'

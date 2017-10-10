6e8b7b83e1e3c4cfb987685881f84d3b332b1664
Kianutama Radianur <kinu.kianu@gmail.com>
2017-10-10 14:44:36 +0700

    add several test

--- ntaps_update_status/tests.py
+++ ntaps_update_status/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import resolve
+from .models import Status
+from .views import index
+from .forms import Status_Form
 
 # Create your tests here.
+class UpdateStatusUnitTest(TestCase):
+
+	def test_update_status_url_is_exist(self):
+		response = Client().get('/update_status/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_update_status_using_index_func(self):
+		found = resolve('/update_status/')
+		self.assertEqual(found.func, index)
+
+	def test_model_can_create_new_status_and_has_str_function(self):
+		new_activity = Status.objects.create(status='menulis status harian')
+		counting_all_available_status=Status.objects.all().count()
+		self.assertEqual(counting_all_available_status,1)
+		self.assertEqual(str(new_activity), new_activity.status)
+
+	def test_form_status_input_has_placeholder_and_css_classes(self):
+	    form = Status_Form()
+	    self.assertIn('class="status-form-textarea', form.as_p())
+	    self.assertIn('id="id_status', form.as_p())
+
+	def test_form_validation_for_blank_items(self):
+	    form = Status_Form(data={'status':''})
+	    self.assertFalse(form.is_valid())
+	    self.assertEqual(
+	        form.errors['status'],
+	        ["This field is required."]
+	    )
+	def test_update_status_post_success_and_render_the_result(self):
+	    test = 'Anonymous'
+	    response_post = Client().post('/update_status/add_status', {'status': test})
+	    self.assertEqual(response_post.status_code, 302)
+
+	    response= Client().get('/update_status/')
+	    html_response = response.content.decode('utf8')
+	    self.assertIn(test, html_response)
+
+	def test_lab5_post_error_and_render_the_result(self):
+	    test = 'Anonymous'
+	    response_post = Client().post('/update_status/add_status', {'status': ''})
+	    self.assertEqual(response_post.status_code, 302)
+
+	    response= Client().get('/update_status/')
+	    html_response = response.content.decode('utf8')
+	    self.assertNotIn(test, html_response)

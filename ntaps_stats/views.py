089207204632a238f6f4423328e49f426c25a19a
Mohamad Mahendra <m.mahen030@gmail.com>
2017-10-13 03:10:51 +0700

    added font

--- ntaps_stats/views.py
+++ ntaps_stats/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-response = {'name': "Hash Slinging Slasher",'friends' : "120 people", 'feed' : "23 Post"}
+response = {'name': "Hash Slinging Slasher",'friends' : "666 people", 'feed' : "69(LOL) Post"}
 def index(request):
     html = 'stats/ntaps_stats.html'
     return render(request, html, response)

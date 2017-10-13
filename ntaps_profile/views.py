d9bff56586cc22c9133f98383ef225c0e4e29cd9
Mochaul <praditomo@gmail.com>
2017-10-13 15:14:28 +0700

    edit views

--- ntaps_profile/views.py
+++ ntaps_profile/views.py
@@ -3,18 +3,24 @@ from django.http import HttpResponseRedirect
 from .models import Profile
 
 # Create your views here.
-response = {'name': "Hash Slinging Slasher", 'birthday': "29 Feb", 'gender': "Unknown", 'description': "A dead Krusty Krab's cook", 'email': "hashslinging@krustykrab.bb"}
+#response = {'name': "Hash Slinging Slasher", 'birthday': "29 Feb", 'gender': "Unknown", 'description': "A dead Krusty Krab's cook", 'email': "hashslinging@krustykrab.bb"}
 expertise = ["Rusty spatula", "Red eyes", "A cook", "Missing", "Ghost", "Clumsy"]
+name = 'Hash Slinging Slasher'
+birthday = '29 Feb'
+gender = 'Unknown'
+description = "A dead krusty krab's cook"
+email = 'hashslinging@krustykrab.bb'
+profile = Profile(name = 'Hash Slinging Slasher', birthday = '29 Feb', gender = 'Unknown', email = 'hashslinging@krustykrab.bb', description = "A dead krusty krab's cook", expertise = ["Rusty spatula", "Red eyes", "A cook", "Missing", "Ghost", "Clumsy"])
+profile.save()
+response = {}
 def index(request):
-
-    #terus digimanain
-    name = Profile.name
-    birthday = Profile.birthday
-    gender = Profile.gender
-    expertise = Profile.expertise
-    email = Profile.email
-    description = Profile.description
+    response['name'] = name
+    response['birthday'] = birthday
+    response['email'] = email
+    response['description'] = description
+    response['gender'] = gender
+    response['expertise'] = expertise
 
     html = 'profile/ntaps_profile.html'
-    response['expertise'] = expertise
+
     return render(request, html, response)

d7ffadded717f113fe93ec1bc1015844c3cf00bc
Mohamad Mahendra <m.mahen030@gmail.com>
2017-10-13 19:51:25 +0700

    edited

--- ntaps_stats/views.py
+++ ntaps_stats/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from ntaps_add_friend.models import Friend
 from ntaps_update_status.models import Status
-from ntaps_profile.models import Profile
+from ntaps_profile.views import name
 
 response = {}
 def index(request):
     response['friend_total'] = Friend.objects.count()
     response['status_total'] = Status.objects.count()
     response['latest_status'] = Status.objects.last()
-    response['profile'] = Profile
+    response['name'] = name
     html = 'stats/ntaps_stats.html'
     return render(request, html, response)
 
